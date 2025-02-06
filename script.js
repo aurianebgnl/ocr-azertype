177f5b0d8e52e9bdb367dababb51bf5d79d673d7
aurianebgnl <auriane.bgnl@gmail.com>
2025-02-06 16:34:19 +0100

    ajout function choisirPhrasesOuMots

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-function afficherResultat(score, nbPropositions) {
-    let message = 'Votre score est de ' + score + ' sur ' + nbPropositions
+function afficherResultat(score, nbEssais) {
+    let message = 'Votre score est de ' + score + ' sur ' + nbEssais
     return message
     }
 
+function choisirPhrasesOuMots() {
+    let monChoix = prompt("Veuillez choisir la liste : phrases ou mots ?", "\"phrases\" ou \"mots\"")
+    while (monChoix !== "phrases" && monChoix !== "mots") {
+        monChoix = prompt("Veuillez choisir la liste : phrases ou mots ?", "\"phrases\" ou \"mots\"")
+        }
+    return monChoix
+    }
+    
 const listeMots = ['Pétunia', 'Cachalot', 'Bourbouze']
 const listePhrases = ['Bonjour, comment allez-vous ?', 'Salut, ça va ?', 'Coucou, ça va ?']
 
-let score = 0
+let monScore = 0
+let mesEssais = 0
 
-let monChoix = prompt("Veuillez choisir la liste : phrases ou mots ?", "\"phrases\" ou \"mots\"")
-while (monChoix !== "phrases" && monChoix !== "mots") {
-monChoix = prompt("Veuillez choisir la liste : phrases ou mots ?", "\"phrases\" ou \"mots\"")
-}
-
-if (monChoix === "mots" ) {
+if (choisirPhrasesOuMots() === "mots" ) {
     for (let i = 0; i < listeMots.length; i++) {
         let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
         if (motUtilisateur === listeMots[i]) {
-            score ++
-            console.log(afficherResultat(score,listeMots.length))
+            monScore ++
+            mesEssais ++
+            console.log(afficherResultat(monScore,mesEssais))
         }
     }
 } else { 
     for (let i = 0; i < listePhrases.length; i++) {
         let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
         if (phraseUtilisateur === listePhrases[i]) {
-            score ++
-            console.log(afficherResultat(score,listePhrases.length))
+            monScore ++
+            mesEssais ++
+            console.log(afficherResultat(monScore,mesEssais))
         }
     }
     
