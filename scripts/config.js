1f9a709300ac645c002d645ed0bdeb44fbef5307
aurianebgnl <auriane.bgnl@gmail.com>
2025-02-07 14:29:39 +0100

    ajout .css et .html à jour

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 // Liste des mots utilisés pour le jeu
 const listeMots = ['Pétunia', 'Cachalot', 'Bourbouze']
-const listePhrases = ['Bonjour, comment allez-vous ?', 'Salut, ça va ?', 'Coucou, ça va ?']
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-lancerJeu()
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
+// lancerJeu()
+
+let baliseZoneSaisie = document.getElementById("inputEcriture");
+let baliseBouton = document.getElementById("btnValiderMot");
+
+console.log(baliseZoneProposition, baliseBouton);
