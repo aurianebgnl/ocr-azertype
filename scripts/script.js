d04cdc7fd09a9c041e8dcfb50a40b67aadbc67f1
aurianebgnl <auriane.bgnl@gmail.com>
2025-03-04 16:44:36 +0100

    ajout choix Mots ou Phrases

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,62 @@
 
-function afficherResultat(score, nbPropositions) {
-    let spanScore = document.querySelector(".zoneScore span")
-    spanScore.innerText= `${score} / ${nbPropositions}`
-    console.log('Votre score est de ' + score + ' sur ' + nbPropositions)
-}
 
 function afficherProposition (afficherMot) {
     let divProposition = document.querySelector(".zoneProposition");
     divProposition.innerText = afficherMot;
 }
 
+function afficherResultat(score, nbPropositions) {
+    let spanScore = document.querySelector(".zoneScore span")
+    spanScore.innerText= `${score} / ${nbPropositions}`
+    console.log('Votre score est de ' + score + ' sur ' + nbPropositions)
+}
+
 function lancerJeu () {
     let i = 0;
     let score = 0;
+    let listeProposition = listeMots;
     
     let btnValiderMot = document.querySelector("#btnValiderMot");
     let inputEcriture = document.querySelector("#inputEcriture");
+    let listeRadioOptions = document.querySelectorAll('.optionSource input')
 
-    afficherProposition(listeMots[i]);
+    // choixInitial();
+    afficherProposition(listeProposition[i]);
     afficherResultat(score, i);
 
     btnValiderMot.addEventListener('click', () => {
         
-        if (inputEcriture.value === listeMots[i]) {
+        if (inputEcriture.value === listeProposition[i]) {
             score++
         };
         i ++;
         afficherResultat(score, i);
         inputEcriture.value = "";
         
-        if (i < listeMots.length){
-            afficherProposition(listeMots[i])
-            // console.log(`à taper : ${listeMots[i]}`);
+        if (i < listeProposition.length){
+            afficherProposition(listeProposition[i])
+            
         } else {
             afficherProposition("Le jeu est terminé !");
             btnValiderMot.disabled = true;
-            // console.log("c'est fini")
+           
         } 
         
         afficherResultat(score, i);
-        
+
     });
 
+    for (let index = 0;index < listeRadioOptions.length; index++) {
+        listeRadioOptions[index].addEventListener('change', (event) => {
+            // pour lire la valeur du bouton selectionné 
+            console.log(event.target.value)
+            if (event.target.value === 1) {
+                listeProposition = listeMots
+            } else {
+                listeProposition = listePhrases
+            }
+            afficherProposition(listeProposition[i])
+        })
+    
+    }
 }
-
